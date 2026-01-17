a079af31c0a4b0df06d27d6f3818a5a97e167930
colemanimation <111820320+colemanimation@users.noreply.github.com>
2026-01-17 01:29:08 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,8 @@ export const CONFIG = {
   defaultMap: "world_continent",
   defaultSpawn: { x: 40, y: 18, facing: "down" }, // foot tile coords
   useUrlMapOverride: true,
+  
+    // Camera viewport in tiles (characters)
+  viewWidth: 48,
+  viewHeight: 22,
 };
